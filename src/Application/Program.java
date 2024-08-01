e74db96bd3ee86dbb335063dbdc5af65e4f716c9
Túlio Dantas <tuliopragana@hotmail.com>
2024-08-01 19:56:36 -0300

    Projeto criado

---
+++ src/Application/Program.java
@@ -0,0 +1,9 @@
+package Application;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        }
+    }
