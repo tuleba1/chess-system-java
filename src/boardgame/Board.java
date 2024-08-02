06ec0294612be77aac428dd0419a8be5c52410c6
Túlio Dantas <tuliopragana@hotmail.com>
2024-08-01 22:06:13 -0300

    Exceções de tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece [][] pieces;
 
     public Board(int columns, int rows) {
+        if(rows < 1 || columns < 1){
+            throw new BoardException("ERRO AO CRIAR: É necessário que haja pelo menos uma linha e uma coluna.");
+        }
         this.columns = columns;
         this.rows = rows;
         pieces = new Piece[rows][columns];
@@ -15,26 +18,42 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
     public Piece piece(int row, int column){
+        if(!positionExists(row, column)){
+            throw new BoardException("ERRO: não está no tabuleiro.");
+        }
         return pieces[row][column];
 
     }
     public Piece piece (Position position){
+        if(!positionExists(position)){
+            throw new BoardException("ERRO: não está no tabuleiro.");
+        }
+
         return pieces[position.getRow()][position.getColumn()];
     }
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new BoardException("ERRO: Já existe uma peça nesta posição"+ position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+    public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("ERRO: não está no tabuleiro.");
+        }
+
+        return piece(position) != null;
     }
 }
